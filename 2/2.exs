89a9f7d910fd59923e5229533cd613bc43b41cb9
Vadim Kulikov <kulikov.vs97@mail.ru>
2024-12-03 20:35:49 +0300

    Second part of second day task, rewrite code

--- 2/2.exs
+++ 2/2.exs
@@ -1,37 +1,34 @@
 defmodule AOC.D2 do
+  def is_safe(report) do
+    check_diff = fn x, y -> abs(x - y) in 1..3 end
+    chunks = report |> Enum.chunk_every(2, 1, :discard)
+
+    is_increasing = chunks |> Enum.all?(fn [x, y] -> check_diff.(x, y) and x < y end)
+    is_decreasing = chunks |> Enum.all?(fn [x, y] -> check_diff.(x, y) and y < x end)
+
+    is_increasing or is_decreasing
+  end
+
   def first() do
-    res =
-      File.stream!("input.txt")
-      |> Stream.map(&String.trim/1)
-      |> Enum.to_list()
-      |> Enum.map(fn str -> String.split(str) |> Enum.map(&String.to_integer/1) end)
-      |> Enum.filter(fn list ->
-        chunks =
-          list
-          |> Enum.chunk_every(2, 1)
+    File.stream!("input.txt")
+    |> Stream.map(&String.trim/1)
+    |> Enum.to_list()
+    |> Enum.map(fn str -> String.split(str) |> Enum.map(&String.to_integer/1) end)
+    |> Enum.count(&is_safe/1)
+    |> IO.inspect()
+  end
 
-        check_diff = fn x, y -> 1 <= abs(x - y) and abs(x - y) <= 3 end
-
-        is_increasing =
-          chunks
-          |> Enum.all?(fn
-            [x, y] -> check_diff.(x, y) and x < y
-            [_] -> true
-          end)
-
-        is_decreasing =
-          chunks
-          |> Enum.all?(fn
-            [x, y] -> check_diff.(x, y) and y < x
-            [_] -> true
-          end)
-
-        is_increasing or is_decreasing
-      end)
-      |> Enum.count()
-
-    IO.puts(res)
+  def second() do
+    File.stream!("input.txt")
+    |> Stream.map(&String.trim/1)
+    |> Enum.to_list()
+    |> Enum.map(fn str -> String.split(str) |> Enum.map(&String.to_integer/1) end)
+    |> Enum.count(fn report ->
+      Enum.any?(
+        0..(length(report) - 1),
+        &(report |> List.delete_at(&1) |> is_safe())
+      )
+    end)
+    |> IO.inspect()
   end
 end
-
-AOC.D2.first()
